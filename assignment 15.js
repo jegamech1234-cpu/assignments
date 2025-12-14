9eeef4131e7cfc7f6ee8f538e64f658a15c2f3eb
jegamech1234-cpu <jegamech1234@gmail.com>
2025-12-14 23:17:50 +0530

    sla

---
+++ assignment 15.js
@@ -0,0 +1,48 @@
+console.log(5 + 3 * 2);
+console.log((10 - 4) / 2);
+console.log(7 % 3);
+console.log(2 ** 3);
+let a = 10;
+a += 5;
+console.log(a);
+
+// Q6.console.log(10 > 5);
+// Q7.console.log(10 >= 10);
+// Q8.console.log(4 == "4");
+// Q9.console.log(4 === "4");
+// Q10.console.log(5 != 6);
+
+// Q11.console.log(true && false);
+// Q12.console.log(false || true);
+// Q13.console.log(!true);
+// Q14.console.log(10 > 5 && 8 < 3);
+// Q15.console.log(7 < 10 || 10 < 7);
+
+// let x = 8;
+
+// x -= 3;
+
+// console.log(x);
+
+// let y = 4;
+
+// y *= 2;
+
+// console.log(y);
+
+// let z = 10;
+
+// z /= 5;
+
+// console.log(z);
+
+// let n = 5;
+
+// n %= 2;
+
+// console.log(n);
+
+// let a = 2,
+//   b = 3;
+
+// console.log(a++ + ++b);
