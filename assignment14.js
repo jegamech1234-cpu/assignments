3b617391262f3897c88c7245729a21c1fa653c79
jegamech1234-cpu <jegamech1234@gmail.com>
2025-12-14 22:22:32 +0530

    sla

---
+++ assignment14.js
@@ -0,0 +1,53 @@
+//  1. String
+let name = "Ramesh";
+let greeting = "Hello, World!";
+console.log(name);
+console.log(greeting);
+
+//  2. Number
+let age = 25;
+let price = 99.99;
+console.log(age);
+console.log(price);
+
+//  3. Boolean
+let isStudent = true;
+let hasLicense = false;
+console.log(isStudent);
+console.log(hasLicense);
+
+//  4. Undefined
+let city;
+let country;
+console.log(city);
+console.log(country);
+
+//  5. Null
+let score = null;
+let response = null;
+console.log(score);
+console.log(response);
+
+//  6. Object
+let person = { name: "John", age: 30 };
+let car = { brand: "Hyundai", model: "Creta" };
+console.log(person);
+console.log(car);
+
+//  7. Array
+let colors = ["red", "green", "blue"];
+let numbers = [10, 20, 30];
+console.log(colors);
+console.log(numbers);
+
+//  8. Symbol
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+console.log(id1);
+console.log(id2);
+
+//  9. BigInt
+let bigNum1 = 123456789012345678901234567890n;
+let bigNum2 = BigInt(98765432109876543210);
+console.log(bigNum1);
+console.log(bigNum2);
